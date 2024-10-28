a59cb13367c8fb974dd519ff317e5aca2ad674b9
Esmael <tryit7212@example.com>
2024-10-28 13:30:46 +0300

    final commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,47 @@
 import { Search } from "lucide-react";
 import RecipeCard from "../compponents/RecipeCard";
+import { useEffect, useState } from "react";
+import { getRandomColor } from "../lib/utility";
 function Home() {
+  const APP_ID = import.meta.env.VITE_APP_ID;
+  const APP_KEY = import.meta.env.VITE_APP_KEY;
+
+  const [recipes, setRecipes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchRecipes = async (searchQuery) => {
+    setLoading(true);
+    setRecipes([]);
+    try {
+      const response = await fetch(
+        `https://api.edamam.com/api/recipes/v2?app_id=${APP_ID}&app_key=${APP_KEY}&q=${searchQuery}&type=public`
+      );
+
+      // Check if the response is successful
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      setRecipes(data.hits);
+      console.log("Data fetched successfully:", data);
+    } catch (error) {
+      console.error("Error fetching data:", error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    fetchRecipes("stew");
+  }, []);
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetchRecipes(e.target[0].value);
+  }
   return (
     <div className="bg-[#faf9fb] p-10 flex-1">
       <div className="max-w-screen-lg mx-auto">
-        <form>
+        <form onSubmit={handleSubmit}>
           <label className="input shadow-md flex items-center gap-2">
             <Search size={24} />
             <input
@@ -21,18 +58,22 @@ function Home() {
           Popular Choices
         </p>
         <div className="grid gap-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-          <RecipeCard />
-          <RecipeCard />
-          <RecipeCard />
+          {!loading &&
+            recipes.map(({ recipe }, index) => (
+              <RecipeCard key={index} recipe={recipe} {...getRandomColor()} />
+            ))}
 
-          <RecipeCard />
-
-          <RecipeCard />
-
-          <RecipeCard />
-
-          <RecipeCard />
-          <RecipeCard />
+          {loading &&
+            [...Array(9)].map((_, index) => (
+              <div key={index} className="flex w-full flex-col gap-4">
+                <div className="skeleton h-32 w-full"></div>
+                <div className="flex justify-between">
+                  <div className="skeleton h-4 w-28"></div>
+                  <div className="skeleton h-4 w-24"></div>
+                </div>
+                <div className="skeleton h-4 w-1/2"></div>
+              </div>
+            ))}
         </div>
       </div>
     </div>
